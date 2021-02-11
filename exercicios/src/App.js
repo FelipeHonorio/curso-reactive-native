136ab54600d829a1a134a97b0e9af37467c2d035
Felipe Honório <felipe.h.camargo@gmail.com>
2021-02-10 21:29:50 -0300

    fist style

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 
 import CompDefault, { Comp1, Comp2 } from './components/Multi'
 import Fist from './components/Fist'
 
 export default () => (
-    <View>
+    <View style={style.App}>
         <CompDefault />
         <Comp1 />
         <Comp2 />
         <Fist />
     </View>
 )
+
+const style = StyleSheet.create({
+    App: {
+        //backgroundColor: '#A00',
+        flexGrow: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
